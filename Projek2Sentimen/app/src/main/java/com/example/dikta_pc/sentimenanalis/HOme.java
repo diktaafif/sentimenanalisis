f96a5b726567a1ac40a52f3b8ebde2f0f90d3aa7
diktaafif <afifdikta@gmail.com>
2019-12-16 00:15:39 +0700

    v

---
+++ Projek2Sentimen/app/src/main/java/com/example/dikta_pc/sentimenanalis/HOme.java
@@ -0,0 +1,14 @@
+package com.example.dikta_pc.sentimenanalis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HOme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
